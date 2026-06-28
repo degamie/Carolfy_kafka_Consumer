499e4e48c7ee68e8d7eb1594961c5f119508b0bd
Degamie_sign <degamiesign@gmail.com>
2026-06-28 09:19:40 +0530

    UserDetailsService#1

--- src/main/java/com/kafka/Carofly/JwtAuthFilter.java
+++ src/main/java/com/kafka/Carofly/JwtAuthFilter.java
@@ -1,10 +1,14 @@
-//WID(26/6/2026(Sarthak Mittal(DegamieSign(JwtAuthFilter#1))
+//WID(28/6/2026(Sarthak Mittal(DegamieSign(JwtAuthFilter#1)).1
 package com.kafka.Carofly;
 
 import org.apache.catalina.filters.HttpHeaderSecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 
-public class JwtAuthFilter extends HttpHeaderSecurityFilter{}
+public class JwtAuthFilter extends HttpHeaderSecurityFilter{
+    @Autowired
+    public UserDetailsService userDetailsService;
+}
