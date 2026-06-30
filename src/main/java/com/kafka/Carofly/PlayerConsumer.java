ce36c9b803f0c84a7e9009e27192ed60287459a4
Degamie_sign <degamiesign@gmail.com>
2026-06-30 14:11:57 +0530

    player_consumer_instance_binding#1

--- src/main/java/com/kafka/Carofly/PlayerConsumer.java
+++ src/main/java/com/kafka/Carofly/PlayerConsumer.java
@@ -1,4 +1,4 @@
-//WID(30/6/2026)(Sarthak Mittal(Carofly_kafka_Consumer_API)#1.1,1.1.1.1.1/1
+//WID(30/6/2026)(Sarthak Mittal(Carofly_kafka_Consumer_API)(Logic)
 package com.kafka.Carofly;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.Carofly.dto.PlayerConsumerdto;
@@ -10,6 +10,7 @@ import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class PlayerConsumer {
+    public void setPlayerconsumer(PlayerConsumer consumer){this.playerconsumer=playerconsumer;}//binding playerconsumer's Instance in GameApp
     PlayerConsumer(String PLAYER_TOPIC){
         this.PLAYER_TOPIC=PLAYER_TOPIC;
     }
