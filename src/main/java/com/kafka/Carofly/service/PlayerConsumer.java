1e4f2a8dcb5dae44ef4fc3089eb3c46ed9d975a7
Degamie_sign <degamiesign@gmail.com>
2026-07-28 15:34:16 +0530

    playyer_ConsumerFactory

--- src/main/java/com/kafka/Carofly/service/PlayerConsumer.java
+++ src/main/java/com/kafka/Carofly/service/PlayerConsumer.java
@@ -1,23 +1,37 @@
-//WID(28/07/2026)(Sarthak Mittal(Carofly_kafka_Consumer_API)(Logic)(player message consume(producer)#1
+//WID(28/07/2026)(Sarthak Mittal(Carofly_kafka_Consumer_API)(Logic)(playyer_ConsumerFactory)#1
 package com.kafka.Carofly.service;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.kafka.Carofly.dto.PlayerConsumerdto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.kafka.support.serializer.JacksonJsonDeserializer.TRUSTED_PACKAGES;
 
 @Service
 @EnableKafka
 @EnableDiscoveryClient
 
 public class PlayerConsumer {
+    Map<String,Object> props=new HashMap<>();
     public ChatClient chatClinet;//Anthropic Ai Chat Client Obj declare
     @Value("$spring.kafka.bootstrap-servers")
     public String bootstrapServers;
@@ -28,10 +42,35 @@ public class PlayerConsumer {
     KafkaTemplate<String,Integer>kafkaTemplate;
     public PlayerConsumer playerconsumer;
 
+    @Bean
+    public ConsumerFactory<String,PlayerConsumerdto>consumerFactory(){
+        JsonDeserializer<PlayerConsumerdto> deserializer = new JsonDeserializer<>(PlayerConsumerdto.class) {
+            @Override
+            public PlayerConsumerdto deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+                return jsonParser+deserializationContext;
+            }
+        };
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "client-chat-ai-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer .class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer .class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer .class);
+        props.put(TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+
+
     //Consuming Player messaage///
     @KafkaListener (topics = "player-topics", groupId = "${spring.kafka.consumer.group-id}")//Kafka Topics and group id declare
     public void consume(String msg, PlayerConsumerdto playerConsumerdto,ChatClient chatClient) throws Exception {//consume method declare
-        Map<String,Object> props=new HashMap<>();
+
         String prompt = "Processing player in the Game: " + playerConsumerdto.getPlayerName();
         String response=chatClient.prompt(prompt).call().content();
         System.out.println("Recieving Player Message from Kafka: {}" +msg +response);//Printing Recieved Player's
