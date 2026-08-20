172174c5469f4abc6c7f205be421c1e43155dbce
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-20 18:13:36 +0530

    Add files via upload

--- src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
+++ src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
@@ -1,18 +1,18 @@
-//WID(15/8/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)
+//WID(20/8/2026)(Sarthak Mittal(Degamiesign)
 package com.kafka.Carofly.service;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 @Service
 public class PlayerBroadCastService {
-    public JwtUtil jwtUtil;
+    public void setSimpMessagingTemplate(SimpleMessagingTemplate simpMessagingTemplate){
+        this.simpMessagingTemplate=simpMessagingTemplate;
+    }
     public PlayerBroadCastService(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
     }
+
     SimpMessagingTemplate simpMessagingTemplate;
-    void setSimpMessagingTemplate(SimpMessagingTemplate simpMessagingTemplate){
-        this.simpMessagingTemplate=simpMessagingTemplate;
-    }
     public final SimpMessagingTemplate getSimpleMessageTemplate() {
         return simpMessagingTemplate;
     }
