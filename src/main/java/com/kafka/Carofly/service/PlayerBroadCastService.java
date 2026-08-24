6e6dd2b977220600e5c870c0302596a4528234d0
unknown <degamiesign@gmail.com>
2026-08-24 18:37:22 +0530

    Binding jwtutil

--- src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
+++ src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
@@ -1,10 +1,13 @@
-//WID(15/8/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)
+//WID(24/8/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)
 package com.kafka.Carofly.service;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 @Service
 public class PlayerBroadCastService {
+    void setJwtUtil(JwtUtil jwtUtil){
+        this.jwtUtil=jwtUtil;
+    }
     public JwtUtil jwtUtil;
     public PlayerBroadCastService(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
