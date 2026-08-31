8da3ffe341143f663c9323f2ab36a6ed79898572
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-31 15:18:56 +0530

    Add files via upload

--- src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
+++ src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
@@ -1,23 +1,30 @@
-//WID(28/8/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)
+//WID(31/8/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)#1
 package com.kafka.Carofly.service;
 
-import com.kafka.Carofly.dto.ChatMessage;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 @Service
 public class PlayerBroadCastService {
-    @AutoWired
-    public ChatMessage chatMessage;
-    ChatMessage getChatMessage(ChatMessage chatMessage){return chatMessage;}
-    void setChatMessage(ChatMessage chatMessage){this.chatMessage=chatMessage;}
-    public void setSimpMessagingTemplate(SimpleMessagingTemplate simpMessagingTemplate){
-        this.simpMessagingTemplate=simpMessagingTemplate;
+    void updateByjwtUtil(JwtUtil jwtUtil){
+        getjwtUtil(jwtUtil)+setJwtUtil(jwtUtil)+1;
     }
+    private JwtUtil getjwtUtil(JwtUtil jwtUtil) {
+        return jwtUtil;
+    }
+
+
+
+    void setJwtUtil(JwtUtil jwtUtil){
+        this.jwtUtil=jwtUtil;
+    }
+    public JwtUtil jwtUtil;
     public PlayerBroadCastService(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
     }
-
     SimpMessagingTemplate simpMessagingTemplate;
+    void setSimpMessagingTemplate(SimpMessagingTemplate simpMessagingTemplate){
+        this.simpMessagingTemplate=simpMessagingTemplate;
+    }
     public final SimpMessagingTemplate getSimpleMessageTemplate() {
         return simpMessagingTemplate;
     }
