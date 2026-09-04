19ab664b8a792faac4bae57c2b9e71ad5cd049a8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-04 22:17:59 +0530

    Add files via upload

---
+++ src/main/java/com/kafka/Carofly/service/PlayerConsumerController.java
@@ -0,0 +1,36 @@
+//WID(4/9/2026)(Sarthak Mittal(DegamieSign(PlayerConsumerCntrller(Param_Constr))#1
+package com.kafka.Carofly.Controller;
+
+import com.kafka.Carofly.dto.PlayerConsumerdto;
+import com.kafka.Carofly.service.PlayerConsumer;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@EnableCaching
+@Controller
+@RestController("/player-consumer")
+
+public class PlayerConsumerController {
+    @Autowired
+    public PlayerConsumer playerConsumer;
+    PlayerConsumerController(PlayerConsumer playerConsumer){
+        this.playerConsumer=playerConsumer;
+    }
+    @GetMapping("/playerid")
+    public String consumeplayerid(@RequestAttribute  String playerconsumerid){
+        return playerConsumer.consumeplayerid(playerconsumerid);
+    }
+//@As
+    @GetMapping("/message")
+    public List<PlayerConsumerdto> consume(@RequestBody PlayerConsumerdto playerConsumerdto, @RequestBody ChatClient chatClient, @RequestBody Acknowledgment ack, @RequestAttribute  String msg) throws Exception {//consume method declare
+        return playerConsumer.consume(msg,playerConsumerdto,chatClient,ack);
+    }
+
+}
