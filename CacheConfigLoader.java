6f4bfde5b7a96293eef5d7a3f0391f391a4ea8b0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-04 16:48:45 +0530

    Add files via upload

---
+++ CacheConfigLoader.java
@@ -0,0 +1,23 @@
+//WID(04/09/2026)(Sarthak Mittal)(CacheConfigLoader)#1.1
+package com.kafka.Carofly.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+@EnableCaching
+@Configuration
+public class CacheConfigLoader{
+    CacheConfigLoader configLoader;
+
+    CacheConfigLoader(CacheConfigLoader configLoader){
+        this.configloader=configLoader;
+    }
+    @Bean
+    public CaffeineCacheManager caffieneCacheManager(){
+        CaffeineCacheManager  caffeineCacheManager=new CaffeineCacheManager();
+        caffeineCacheManager.setCaffiene(caffeineCacheBuilder());
+        return caffeineCacheManager;
+    }
+
+}
