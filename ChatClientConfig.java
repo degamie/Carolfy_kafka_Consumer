f1c984ab2197ec627ac3a822925a428f17a4e1e4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-06 12:37:44 +0530

    Add files via upload

---
+++ ChatClientConfig.java
@@ -0,0 +1,31 @@
+//WID(6/8/2026)(Sarthak Mittal(AICHatClient)#impl.1
+package com.kafka.Carofly.config;
+
+import com.kafka.Carofly.service.ChatClientService;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class ChatClientConfig
+ {
+     @Autowired
+     public ChatClientService chatClientService;
+
+     void setchatClient(ChatClient chatClient){
+         this.chatClient=chatClient;
+     }
+     
+     private  ChatClient chatClient;
+public void setChatClient(ChatClient  chatClient){
+    this.chatClient=chatClient;
+}
+//     public ChatClientConfig(ChatClient chatClient){
+//         this.chatClient=chatClient;
+//     }
+
+     @Bean
+     public ChatClient chatClient(ChatClient.Builder builder){
+         return builder.build();
+     }
+}
