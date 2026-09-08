169585926dfccdaaf0a0f32d5c748143c5b722a2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-08 07:29:15 +0530

    Add files via upload

---
+++ ChatMessage.java
@@ -0,0 +1,44 @@
+//WID(8/8/2026)(Sarthak Mittal)(DegamieSign)
+package com.kafka.Carofly.dto;
+
+import com.kafka.Carofly.service.PlayerConsumer;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class ChatMessage {
+    public DateFormat recieveddate;
+    private String clientId;
+    private String sessionId;
+    private String message;
+    private long timestamp;
+
+    // getters, setters, no-arg constructor
+    public ChatMessage() {}
+
+    public ChatMessage(String clientId, String sessionId, String message, long timestamp) {
+        this.clientId = clientId;
+        this.sessionId = sessionId;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getClientId() { return clientId; }
+    public void setClientId(String clientId) { this.clientId = clientId; }
+
+    public String getSessionId() { return sessionId; }
+    public void setSessionId(String sessionId) { this.sessionId = sessionId; }
+
+    public String getMessage() { return message; }
+    public String setMessage(String message) { this.message = message;
+        return message;
+    }
+
+    public long getTimestamp() { return timestamp; }
+    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+    void setRecieveddate(DateFormat recieveddate){
+        this.recieveddate=recieveddate;
+    }
+
+    }
+//}
