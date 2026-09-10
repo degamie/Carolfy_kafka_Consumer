990afbe7dce77759eeac8ed466017c90bf8acaf6
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-10 12:17:51 +0530

    Add files via upload

---
+++ src/main/java/com/kafka/Carofly/config/JwtUtil.java
@@ -0,0 +1,10 @@
+//WID(10.09.2026)(Sarthak Mittal)(JwtUtil)#1
+package com.kafka.Carofly.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtUtil {
+    public JwtUtil jwtUtil;
+    void setJwtUtil(JwtUtil jwtUtil){this.jwtUtil=jwtUtil;}
+}
