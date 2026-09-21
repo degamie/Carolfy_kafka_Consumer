45c47d3b71c8b410ad7bc594c72ad9c69e657f5f
unknown <degamiesign@gmail.com>
2026-09-21 10:14:20 +0530

    binding#eventType

--- src/main/java/com/kafka/Carofly/service/PlayerBroadCasttRecordEvent.java
+++ src/main/java/com/kafka/Carofly/service/PlayerBroadCasttRecordEvent.java
@@ -1,5 +1,10 @@
+//WD(21/9/2026)(Sarthak Mittal(DegamieSign)(PlayerBroadCasttRecordEvent)
 package com.kafka.Carofly.service;
 
 import java.time.Instant;
 
-public record PlayerBroadCasttRecordEvent(String eventType, Object payload, Instant timestamp) {}
+public record PlayerBroadCasttRecordEvent(String eventType, Object payload, Instant timestamp) {
+    void setEventType(String eventType){
+        this.eventType=eventType;
+    }
+}
