0cf04f47259a536b841bf4cd1299793af3f4a799
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-25 12:49:20 +0530

    Add files via upload

--- PlayerEventRecord.java
+++ PlayerEventRecord.java
@@ -1,4 +1,4 @@
-//WID(7/9/2026)(Satthak Mittal)(DegamieSign)#PlayerEventRecord#impl
+//WID(25/9/2026)(Satthak Mittal)(DegamieSign)#PlayerEventRecord#impl/1
 package com.kafka.Carofly.dto;
 
 import jakarta.persistence.*;
@@ -8,21 +8,25 @@ import java.time.Instant;
 @Entity
 @Table(name="PLAYER")
 public class PlayerEventRecord {
+    private float playerpayloadnumber;
+
     PlayerEventRecord(Long id){
         this.id=id;
     }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String key;
     private String payload;
+    public void setplayerpayloadnumber(float playerpayloadnumber){
+        this.playerpayloadnumber=playerpayloadnumber;
+    }
     private long kafkaOffset;
     private int kafkaPartition;
     private Instant consumedAt;
     public void setKafkaOffset(long kafkaOffset){
         this.kafkaOffset=kafkaOffset;
     }
+
 
 }
