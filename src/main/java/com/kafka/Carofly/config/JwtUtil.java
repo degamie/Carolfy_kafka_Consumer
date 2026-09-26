419fd4de91b66f63b3b7a4580a0ec001aea8c35d
unknown <degamiesign@gmail.com>
2026-09-26 14:31:16 +0530

    fethcing#jwtsigningkey

--- src/main/java/com/kafka/Carofly/config/JwtUtil.java
+++ src/main/java/com/kafka/Carofly/config/JwtUtil.java
@@ -1,10 +1,17 @@
-//WID(10.09.2026)(Sarthak Mittal)(JwtUtil)#1
+//WID(26.09.2026)(Sarthak Mittal)(JwtUtil)#11
 package com.kafka.Carofly.config;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtUtil {
+   private static final String SECRETKEY = "nZv7vIGZ6n/LzZhNCUYFQtcVyxZu+1JKiTQI7P6qPUk=";
     public JwtUtil jwtUtil;
     void setJwtUtil(JwtUtil jwtUtil){this.jwtUtil=jwtUtil;}
+    private Key getSigningKey(){
+        Byte[] byteskey=Decoders.BASE64.decode(SECRETKEY);
+        return Keys.hmacShaKeyFor(byteskey);
+    }
 }
