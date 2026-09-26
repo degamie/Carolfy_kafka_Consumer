c84bed8434090f7ac386e1d3fd468bba3194f827
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-09-26 11:43:52 +0530

    Add files via upload

--- src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
+++ src/main/java/com/kafka/Carofly/service/PlayerBroadCastService.java
@@ -1,17 +1,50 @@
-//WID(22/09/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)#1.1.1
+//WID(26/09/2026)(Sarthak Mittal(Degamiesign)(PlayerBroadCastService)#(handling player messages)
 package com.kafka.Carofly.service;
 
 import com.kafka.Carofly.dto.ChatMessage;
+import com.kafka.Carofly.dto.PlayerConsumerdto;
+import org.apache.kafka.common.KafkaException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.InterruptedIOException;
+import java.util.stream.Stream;
 @Service
+@Transactional(readOnly = true)
+
 public class PlayerBroadCastService {
     public WebClient webClient;
     @Autowired
     public ChatMessage chatMessage;
+    @Autowired
+    PlayerConsumerdto playerConsumerdto;
+    public void handleplayerMessage(String playermessages){
+        String playerid=null;
+        if(playermessages.length()!=0){
+            playerConsumerdto.setplayermessage(String.valueOf(playermessages.matches(playerid)));
+        }
+        else playerConsumerdto.setplayermessage(null);
+        System.out.println(playermessages);
+    }
+    public void readplayerMessage(ChatMessage playermessage)  throws InterruptedIOException{
+        try {
+            setChatMessage(playermessage);
+           Stream playerstream= Stream.of(playermessage)
+                    .map(x->playermessage.length()%2==0)
+                    .sorted(playermessage);
+            System.out.println(playerstream);
+        } catch (KafkaException e) {
+                e.printStackTrace();
+        }
+        finally {
+            String client = "";
+            System.out.println(playermessage.setClientId(client));
+        }
+    }
+//    void updateBychat
     void setWebClient(WebClient webClient){
         this.webClient=webClient;
     }
